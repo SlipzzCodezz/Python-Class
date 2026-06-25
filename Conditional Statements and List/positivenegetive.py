871c2716b7d892734941c2784a5d7bd9df65492c
SlipzzCodezz <aathik5547@gmail.com>
2026-06-25 16:16:11 +0500

    Add files via upload

---
+++ Conditional Statements and List/positivenegetive.py
@@ -0,0 +1,8 @@
+# check if the number is positive or negetive
+number =int(input("Enter any number"))
+if number>0:
+    print("positive")
+elif number<0:
+    print("negetive")
+elif number==0:
+    print("number is zero")
