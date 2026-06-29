75f2b5de5eee62ad88775a6e319ea1c455bce952
SlipzzCodezz <aathik5547@gmail.com>
2026-06-29 17:23:46 +0500

    Add files via upload

---
+++ Loops/tableof14.py
@@ -0,0 +1,8 @@
+print("table of 14")
+
+num=14
+
+for i in range(1,11):
+    mul = num * i
+    print(num,"x",i,"=", mul)
+    
---
+++ Loops/whileloops.py
@@ -0,0 +1,6 @@
+num=1
+sum=0
+while num <=10:
+    sum=sum+num
+    num=sum+1
+    print("Sum of first natural 10 numbers:" ,sum)
